a71ba3aa43067f4f66ae46513e65e4868c67ef4d
victor santos <victor.santos@tce.ma.gov.br>
2024-08-25 18:54:42 -0300

    feat: create player

--- mobilegame/Assets/src/scripts/ui/LoginUI.cs
+++ mobilegame/Assets/src/scripts/ui/LoginUI.cs
@@ -4,23 +4,31 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using TMPro;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class LoginUI : MonoBehaviour
 {
-
-
     private Scene scene;
     public GameObject[] canvas;
     public GameObject canvasLogin;
     public GameObject canvasPassword;
+    public GameObject canvasNewUsername;
+    public GameObject canvasNewEmail;
+    public GameObject canvasNewPassword;
+    public GameObject canvasNewPasswordConfirm;
 
     private string login;
     private string password;
+    private string newUsername;
+    private string newPassword;
+    private string newPasswordConfirm;
+    private string newEmail;
 
-    public Animator uiAnimator;
+    public Animator cameraAnimator;
+    private string apiUrl = "http://localhost:8080/api/players";
 
     public void handleSubmitButton()
     {
@@ -37,15 +45,79 @@ public class LoginUI : MonoBehaviour
         }
     }
 
+    public void handleSignUpSubmitButton()
+    {
+        newUsername = canvasNewUsername.transform.GetChild(0).GetChild(2).GetComponent<TextMeshProUGUI>().text;
+        newPassword = canvasNewPassword.transform.GetChild(0).GetChild(2).GetComponent<TextMeshProUGUI>().text;
+        newPasswordConfirm = canvasNewPasswordConfirm.transform.GetChild(0).GetChild(2).GetComponent<TextMeshProUGUI>().text;
+        newEmail = canvasNewEmail.transform.GetChild(0).GetChild(2).GetComponent<TextMeshProUGUI>().text;
+
+        bool isPasswordValid = Regex.Replace(newPassword, @"\p{C}+", "").Trim() == Regex.Replace(newPasswordConfirm, @"\p{C}+", "").Trim();
+        bool isEmailValid = Regex.IsMatch(newEmail, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+        if (!isPasswordValid)
+        {
+            UnityEngine.Debug.Log("Passwords do not match");
+            return;
+        }
+
+        if (!isEmailValid)
+        {
+            UnityEngine.Debug.Log("Invalid email");
+            return;
+        }
+
+        StartCoroutine(signUpRequest(newUsername, newPassword, newEmail));
+    }
+
     public void handleNewPlayerButton()
     {
-        uiAnimator.SetBool("signup", true);
+        cameraAnimator.SetBool("signup", true);
     }
 
     public void handleBackButton()
     {
-        uiAnimator.SetBool("signup", false);
+        cameraAnimator.SetBool("signup", false);
+    }
+
+
+    [Serializable]
+    class requestBody
+    {
+        public string username;
+        public string email;
+        public string password;
+    };
+    private IEnumerator signUpRequest(string newUsername, string newPassword, string newEmail)
+    {
+        requestBody requestBody = new requestBody
+        {
+            username = newUsername,
+            email = newEmail,
+            password = newPassword
+        };
+
+        string jsonData = JsonUtility.ToJson(requestBody);
+
+        UnityWebRequest request = new UnityWebRequest(apiUrl, "POST");
+        byte[] bodyRaw = new System.Text.UTF8Encoding().GetBytes(jsonData);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            UnityEngine.Debug.LogError("Erro na requisição: " + request.error);
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Resposta recebida: " + request.downloadHandler.text);
+        }
     }
 
 
 }
+
+
