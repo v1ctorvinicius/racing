74a5d735dcce2c41a6538a29eccb461cc6dd0695
victor santos <victor.santos@tce.ma.gov.br>
2024-08-25 14:03:04 -0300

    feat: auth animations

--- mobilegame/Assets/src/scripts/ui/LoginUI.cs
+++ mobilegame/Assets/src/scripts/ui/LoginUI.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -16,29 +17,34 @@ public class LoginUI : MonoBehaviour
     public GameObject canvasLogin;
     public GameObject canvasPassword;
 
-    private String login;
-    private String password;
+    private string login;
+    private string password;
+
+    public Animator uiAnimator;
 
     public void handleSubmitButton()
     {
-
         login = canvasLogin.transform.GetChild(0).GetChild(2).GetComponent<TextMeshProUGUI>().text;
-        password = canvasPassword.transform.GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>().text;
+        password = canvasPassword.transform.GetChild(0).GetChild(2).GetComponent<TextMeshProUGUI>().text;
 
-        
-
-        // StartCoroutine(GetDataFromAPI("https://localhost:8080/players"));
-
-        // if (login == "" || password == "")
-        // {
-        //     return;
-        // }
+        login = Regex.Replace(login, @"\p{C}+", "").Trim();
+        password = Regex.Replace(password, @"\p{C}+", "").Trim();
 
         if (login == "admin" && password == "admin")
         {
+            UnityEngine.Debug.Log("Login successful");
             SceneManager.LoadScene("test_track");
         }
+    }
 
+    public void handleNewPlayerButton()
+    {
+        uiAnimator.SetBool("signup", true);
+    }
+
+    public void handleBackButton()
+    {
+        uiAnimator.SetBool("signup", false);
     }
 
 
